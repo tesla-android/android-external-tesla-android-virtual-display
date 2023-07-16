2096f120dbf842c6c060807acd9805f6bc6cb5fe
Michał Gapiński <mike@gapinski.eu>
2023-07-16 18:05:44 +0200

    Add configurable h264 streaming

--- tesla-android-virtual-display.cpp
+++ tesla-android-virtual-display.cpp
@@ -42,90 +42,128 @@
 
 #include "utils/thread_safe_queue.h"
 
+#include <cutils/properties.h>
+
 #include <ws.h>
 
 using namespace android;
 
-const int encoder_pool_size = 2;
+const int encoder_pool_size = 1;
 
-ThreadSafeQueue < us_frame_s > capture_queue;
-ThreadSafeQueue < encoded_frame_set > encoded_queue;
-ThreadSafeQueue < encoder_set > encoders_pool;
+ThreadSafeQueue<us_frame_s> capture_queue;
+ThreadSafeQueue<encoded_frame_set> encoded_queue;
+ThreadSafeQueue<encoder_set> encoders_pool;
 
-encoder_set create_encoder_set(int num) {
-    encoder_set encoders;
+int isH264 = 0;
 
-    std::string encoder_name_jpeg = "encoder_jpeg" + std::to_string(num);
-    //std::string encoder_name_h264 = "encoder_h264" + std::to_string(num);
-
-    //encoders.h264_encoder = us_m2m_h264_encoder_init(encoder_name_h264.c_str(), "/dev/video11", 1000, 30);
-    encoders.jpeg_encoder = us_m2m_jpeg_encoder_init(encoder_name_jpeg.c_str(), "/dev/video31", 90);
-    return encoders;
+int get_system_property_int(const char *prop_name)
+{
+  char prop_value[PROPERTY_VALUE_MAX];
+  if (property_get(prop_name, prop_value, nullptr) > 0)
+  {
+    return atoi(prop_value);
+  }
+  else
+  {
+    return -1;
+  }
 }
 
-void init_encoder_pool(int pool_size) {
-  for (int i = 0; i < pool_size; i++) {
+encoder_set create_encoder_set(int num)
+{
+  encoder_set encoders;
+
+  if (isH264)
+  {
+    std::string encoder_name_h264 = "encoder_h264" + std::to_string(num);
+    encoders.h264_encoder = us_m2m_h264_encoder_init(encoder_name_h264.c_str(), "/dev/video11", 15000, 45);
+  }
+  else
+  {
+    std::string encoder_name_jpeg = "encoder_jpeg" + std::to_string(num);
+    encoders.jpeg_encoder = us_m2m_jpeg_encoder_init(encoder_name_jpeg.c_str(), "/dev/video31", 90);
+  }
+
+  return encoders;
+}
+
+void init_encoder_pool(int pool_size)
+{
+  for (int i = 0; i < pool_size; i++)
+  {
     encoders_pool.push(create_encoder_set(i));
   }
 }
 
-void take_screenshot(PhysicalDisplayId & displayId, us_frame_s & frame) {
-  void * base = NULL;
+void take_screenshot(PhysicalDisplayId &displayId, us_frame_s &frame)
+{
+  void *base = NULL;
 
-  sp < SyncScreenCaptureListener > captureListener = new SyncScreenCaptureListener();
+  sp<SyncScreenCaptureListener> captureListener = new SyncScreenCaptureListener();
   status_t result = ScreenshotClient::captureDisplay(displayId, captureListener);
-  if (result != NO_ERROR) {
+  if (result != NO_ERROR)
+  {
     return;
   }
 
-  ScreenCaptureResults captureResults = captureListener -> waitForResults();
-  if (captureResults.result != NO_ERROR) {
+  ScreenCaptureResults captureResults = captureListener->waitForResults();
+  if (captureResults.result != NO_ERROR)
+  {
     return;
   }
   ui::Dataspace dataspace = captureResults.capturedDataspace;
-  sp < GraphicBuffer > buffer = captureResults.buffer;
+  sp<GraphicBuffer> buffer = captureResults.buffer;
 
-  result = buffer -> lock(GraphicBuffer::USAGE_SW_READ_OFTEN, & base);
+  result = buffer->lock(GraphicBuffer::USAGE_SW_READ_OFTEN, &base);
 
-  if (base == nullptr || result != NO_ERROR) {
+  if (base == nullptr || result != NO_ERROR)
+  {
     String8 reason;
-    if (result != NO_ERROR) {
+    if (result != NO_ERROR)
+    {
       reason.appendFormat(" Error Code: %d", result);
-    } else {
+    }
+    else
+    {
       reason = "Failed to write to buffer";
     }
     fprintf(stderr, "Failed to take screenshot (%s)\n", reason.c_str());
     return;
   }
 
-  frame.width = buffer -> getWidth();
-  frame.height = buffer -> getHeight();
-  frame.format = V4L2_PIX_FMT_RGBA32;
-  frame.stride = buffer -> getStride() * bytesPerPixel(buffer -> getPixelFormat());
-  frame.used = static_cast < size_t > (frame.width) * static_cast < size_t > (frame.height) * bytesPerPixel(buffer -> getPixelFormat());
-  frame.data = static_cast < uint8_t * > (malloc(frame.used));
+  frame.width = buffer->getWidth();
+  frame.height = buffer->getHeight();
+  frame.format = V4L2_PIX_FMT_BGR32;
+  frame.stride = buffer->getStride() * bytesPerPixel(buffer->getPixelFormat());
+  frame.used = static_cast<size_t>(frame.width) * static_cast<size_t>(frame.height) * bytesPerPixel(buffer->getPixelFormat());
+  frame.data = static_cast<uint8_t *>(malloc(frame.used));
   memcpy(frame.data, base, frame.used);
 
-  buffer -> unlock();
+  buffer->unlock();
 }
 
-void screenshot_thread() {
-  std::optional < PhysicalDisplayId > displayId = SurfaceComposerClient::getInternalDisplayId();
-  if (!displayId) {
+void screenshot_thread()
+{
+  std::optional<PhysicalDisplayId> displayId = SurfaceComposerClient::getInternalDisplayId();
+  if (!displayId)
+  {
     fprintf(stderr, "Failed to get token for internal display\n");
     exit(1);
   }
 
-  while (true) {
+  while (true)
+  {
     us_frame_s frame;
-    take_screenshot( * displayId, frame);
-    if (frame.data != nullptr) {
+    take_screenshot(*displayId, frame);
+    if (frame.data != nullptr)
+    {
       capture_queue.push(frame);
     }
   }
 }
 
-void encode_frame_jpeg(us_m2m_encoder_s * encoder, const us_frame_s & input_frame, us_frame_s & output_frame) {
+void encode_frame_jpeg(us_m2m_encoder_s *encoder, const us_frame_s &input_frame, us_frame_s &output_frame)
+{
   output_frame.width = input_frame.width;
   output_frame.height = input_frame.height;
   output_frame.format = V4L2_PIX_FMT_JPEG;
@@ -134,14 +172,16 @@ void encode_frame_jpeg(us_m2m_encoder_s * encoder, const us_frame_s & input_fram
   output_frame.data = NULL;
 
   bool force_key = false;
-  int compressionResult = us_m2m_encoder_compress(encoder, & input_frame, & output_frame, force_key);
+  int compressionResult = us_m2m_encoder_compress(encoder, &input_frame, &output_frame, force_key);
 
-  if (compressionResult != 0) {
+  if (compressionResult != 0)
+  {
     fprintf(stderr, "Failed to compress JPEG (error code: %d)\n", compressionResult);
   }
 }
 
-void encode_frame_h264(us_m2m_encoder_s * encoder, const us_frame_s & input_frame, us_frame_s & output_frame) {
+void encode_frame_h264(us_m2m_encoder_s *encoder, const us_frame_s &input_frame, us_frame_s &output_frame)
+{
   output_frame.width = input_frame.width;
   output_frame.height = input_frame.height;
   output_frame.format = V4L2_PIX_FMT_H264;
@@ -150,115 +190,129 @@ void encode_frame_h264(us_m2m_encoder_s * encoder, const us_frame_s & input_fram
   output_frame.data = NULL;
 
   bool force_key = false;
-  int compressionResult = us_m2m_encoder_compress(encoder, & input_frame, & output_frame, force_key);
+  int compressionResult = us_m2m_encoder_compress(encoder, &input_frame, &output_frame, force_key);
 
-  if (compressionResult != 0) {
+  if (compressionResult != 0)
+  {
     fprintf(stderr, "Failed to compress JPEG (error code: %d)\n", compressionResult);
   }
 }
 
-void encode_thread(encoder_set encoders) {
-  while (true) {
+void encode_thread(encoder_set encoders)
+{
+  while (true)
+  {
     us_frame_s input_frame = capture_queue.pop();
-    if (input_frame.data != nullptr) {
-      us_frame_s encoded_frame_jpeg;
-      encode_frame_jpeg(encoders.jpeg_encoder, input_frame, encoded_frame_jpeg);
+    us_frame_s encoded_frame_h264; 
+    us_frame_s encoded_frame_jpeg;
 
-      //us_frame_s encoded_frame_h264;
-      //encode_frame_h264(encoders.h264_encoder, input_frame, encoded_frame_h264);
-
+    if (input_frame.data != nullptr)
+    {
       encoded_frame_set encoded_frames;
-      encoded_frames.jpeg_frame = encoded_frame_jpeg;
-      //encoded_frames.h264_frame = encoded_frame_h264;
+      if (isH264)
+      {
+        encode_frame_h264(encoders.h264_encoder, input_frame, encoded_frame_h264);
+        encoded_frames.h264_frame = encoded_frame_h264;
+      }
+      else
+      {
+        encode_frame_jpeg(encoders.jpeg_encoder, input_frame, encoded_frame_jpeg);
+        encoded_frames.jpeg_frame = encoded_frame_jpeg;
+      }
 
-      if (encoded_frame_jpeg.data != nullptr) {
+      if (encoded_frame_jpeg.data != nullptr || encoded_frame_h264.data != nullptr)
+      {
         encoded_queue.push(encoded_frames);
-      } else {
+      }
+      else
+      {
         std::cout << "encode_thread(): Encoded frame data is null" << std::endl;
       }
 
       free(input_frame.data);
-    } else {
-      std::cout << "encode_thread(): Input frame data is null" << std::endl; 
+    }
+    else
+    {
+      std::cout << "encode_thread(): Input frame data is null" << std::endl;
     }
   }
 }
 
-void stream_frame(const encoded_frame_set & encoded_frames) {
-  ws_sendframe_bin(NULL, reinterpret_cast<const char*>(encoded_frames.jpeg_frame.data), encoded_frames.jpeg_frame.used);
+void stream_frame(const encoded_frame_set &encoded_frames)
+{
+  if (isH264)
+  {
+    ws_sendframe_bin(NULL, reinterpret_cast<const char *>(encoded_frames.h264_frame.data), encoded_frames.h264_frame.used);
+  } else {
+    ws_sendframe_bin(NULL, reinterpret_cast<const char *>(encoded_frames.jpeg_frame.data), encoded_frames.jpeg_frame.used);
+  }
 }
 
-void ws_on_connection_opened(ws_cli_conn_t *client) {
+void ws_on_connection_opened(ws_cli_conn_t *client)
+{
   char *cli;
   cli = ws_getaddress(client);
   printf("Connection opened, addr: %s\n", cli);
 }
 
-void ws_on_connection_closed(ws_cli_conn_t *client) {
+void ws_on_connection_closed(ws_cli_conn_t *client)
+{
   char *cli;
   cli = ws_getaddress(client);
   printf("Connection closed, addr: %s\n", cli);
 }
 
-void ws_on_message(__attribute__ ((unused)) ws_cli_conn_t *client,
-       __attribute__ ((unused)) const unsigned char *msg,
-       __attribute__ ((unused)) uint64_t size,
-       __attribute__ ((unused)) int type) {
+void ws_on_message(__attribute__((unused)) ws_cli_conn_t *client,
+                   __attribute__((unused)) const unsigned char *msg,
+                   __attribute__((unused)) uint64_t size,
+                   __attribute__((unused)) int type)
+{
   ws_ping(NULL, 30);
 }
 
-void stream_thread() {
-  us_frame_s last_frame_jpeg;
-  bool has_last_frame = false;
-  int drop_counter = 0;
-  const int drop_limit = 30;
-
-  while (true) {
+void stream_thread()
+{
+  while (true)
+  {
     encoded_frame_set encoded_frames = encoded_queue.pop();
-
-    if (has_last_frame &&
-        (us_frame_compare(&last_frame_jpeg, &encoded_frames.jpeg_frame)) &&
-        drop_counter < drop_limit) {
-      drop_counter++;
-      free(encoded_frames.jpeg_frame.data);
-      //free(encoded_frames.h264_frame.data);
-      continue;
-    }
-
-    drop_counter = 0;
-
     stream_frame(encoded_frames);
-
-    us_frame_copy(&encoded_frames.jpeg_frame, &last_frame_jpeg);
-    has_last_frame = true;
-
-    free(encoded_frames.jpeg_frame.data);
-    //free(encoded_frames.h264_frame.data);
+    if (isH264)
+    {
+      free(encoded_frames.h264_frame.data);
+    }
+    else
+    {
+      free(encoded_frames.jpeg_frame.data);
+    }
   }
 }
 
+int main(__attribute__((unused)) int argc, __attribute__((unused)) char **argv)
+{
+  ProcessState::self()->setThreadPoolMaxThreadCount(4);
+  ProcessState::self()->startThreadPool();
 
-int main(__attribute__((unused)) int argc, __attribute__((unused)) char ** argv) {
-  ProcessState::self() -> setThreadPoolMaxThreadCount(4);
-  ProcessState::self() -> startThreadPool();
+  isH264 = get_system_property_int("persist.tesla-android.virtual-display.is_h264");
 
   init_encoder_pool(encoder_pool_size);
 
   std::thread t_screenshot(screenshot_thread);
-  std::vector < std::thread > encode_threads(encoder_pool_size);
-  for (int i = 0; i < encoder_pool_size; i++) {
+  std::vector<std::thread> encode_threads(encoder_pool_size);
+  for (int i = 0; i < encoder_pool_size; i++)
+  {
     encode_threads[i] = std::thread(encode_thread, encoders_pool.pop());
   }
   std::thread t_stream(stream_thread);
 
   struct ws_events evs;
-  evs.onopen    = &ws_on_connection_opened;
-  evs.onclose   = &ws_on_connection_closed;
+  evs.onopen = &ws_on_connection_opened;
+  evs.onclose = &ws_on_connection_closed;
   evs.onmessage = &ws_on_message;
   ws_socket(&evs, 9090, 0, 0);
 
   t_screenshot.join();
-  for (auto & t: encode_threads) {
+  for (auto &t : encode_threads)
+  {
     t.join();
   }
   t_stream.join();
